7aadd46f130b15e93161ef345815ef9a3f0dacf4
Greg <gregoryc@yeah.net>
2017-03-06 18:54:55 +0800

    v0.6 完成alert
    添加了遮罩层完成模态，添加jqueryUI使窗口拖动

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@
  * Created by Greg on 2017/3/6.
  */
 require.config({
+    //配置jquery路径，注意不带.js结尾，在html中设置了data-main之后，
+    //会以data-main指定的文件目录为根目录，所以这里的jquery路径没有加js/
     paths : {
-        jquery : ["jquery-3.1.1.min"]
+        jquery : ["jquery-3.1.1.min"],
+        jqueryUI : ["jquery-ui.min"]
     }
 });
 require(["jquery","modal"],function ($, m) {
@@ -12,10 +15,14 @@ require(["jquery","modal"],function ($, m) {
             width : 200,
             height : 250,
             content : "Welcome!",
-            handler : function () {
+            alertHandler : function () {
                 alert("clicked");
             },
-            title : "Notice"
+            closeHandler : function () {
+                alert("close");
+            },
+            title : "Notice",
+            hasCloseBtn : true
         });
     });
 });
--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,20 @@
 /**
  * Created by Greg on 2017/3/6.
  */
-define(['jquery'],function ($) {
+define(['jquery','jqueryUI'],function ($,$UI) {
     function Modal() {
         this.cfg = {
-            width : 500,
+            width : 500,  //窗体宽度和高度，还有cfg.x和cfg.y确定样式里left和top
             height : 300,
-            content : "",
-            handler : function (){},
-            title : "提示"
+            hasMask : true,
+            content : "",  //modal_body中的内容
+            alertHandler : function (){}, //单机确定后的事件函数
+            alertBtnText : "OK",
+            title : "提示", //modal_header中的提示
+            hasCloseBtn : false,
+            closeHandler : function () {},
+            skinClassName : null, //在窗口上添加一个类名，在css添加选择器实现定制某个窗口样式
+            isDraggable : true //是否可以拖动
         };
     }
     Modal.prototype = {
@@ -17,13 +23,23 @@ define(['jquery'],function ($) {
             var boundingBox = $('<div class="modal_boundingBox">'+
                 '<div class="modal_header">'+config.title+'</div>'+
                 '<div class="modal_body">'+config.content+'</div>'+
-                '<div class="modal_footer">'+'<input type="button" value="确定">'+'</div>'+
+                '<div class="modal_footer">'+'<input type="button" value="'+config.alertBtnText+'">'+'</div>'+
                                     '</div>');
+            if (config.hasMask){
+                var mask = $('<div class="modal_mask"></div>');
+                mask.appendTo("body");
+            }
             boundingBox.appendTo("body");
+            if (config.isDraggable){
+                boundingBox.draggable({
+                    handle : boundingBox.find(".modal_header")
+                });
+            }
             var btn = boundingBox.find(".modal_footer input");
             btn.click(function(){
-                config.handler && config.handler();
+                config.alertHandler && config.alertHandler();
                 boundingBox.remove();
+                mask && mask.remove();
                 return false;
             });
             boundingBox.css({
@@ -31,7 +47,19 @@ define(['jquery'],function ($) {
                 height : config.height+"px",
                 left : (config.x || (window.innerWidth - config.width) / 2) + "px",
                 top : (config.y || (window.innerHeight - config.height) / 2) + "px"
-            })
+            });
+            if (config.hasCloseBtn){
+                var closeBtn = $('<span class="modal_closeBtn">X</span>');
+                closeBtn.appendTo(boundingBox);
+                closeBtn.click(function () {
+                    boundingBox.remove();
+                    mask && mask.remove();
+                    config.closeHandler && config.closeHandler();
+                });
+            }
+            if (config.skinClassName) {
+                boundingBox.addClass(config.skinClassName)
+            }
         },
         confirm:function () {},
         prompt:function () {}
